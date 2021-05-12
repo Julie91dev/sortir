22a8187f284d67bdef9f410b8a863e4cbcbe3fa5
Julie P <prodhomme.julie91@gmail.com>
2021-05-12 08:53:08 +0200

    admin

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -33,21 +33,41 @@ class MainController extends AbstractController
         $formSearch = $this->createForm(SearchSortieType::class);
 
         //recupération de toutes les sorties
-        $sorties = $sortieRepository->findAll();
-
+        //$sorties = $manager->getRepository(Sortie::class)->findAll();
         //gestion de l'etat en fonction de la date
-        $this->dateModifieEtat($sortieRepository,$manager);
+        //foreach ($sorties as $sortie) {
 
-
+          //  if ($sortie->getEtat()->getLibelle() != "Annulée" && $sortie->getEtat()->getLibelle() != "Creer") {
+            //    $this->dateModifieEtat($sortieRepository, $manager);
+            //}
+        //}
+        $this->dateModifieEtat($sortieRepository, $manager);
         //filtre sur les sorties - traitement du formualaire
             //recupération des info dans la requete
              $formSearch->handleRequest($request);
+        //affichage de base
+
+        $campusSelect = 4;
+        $userId = $userInterface->getId();
+        $search = null;
+        $dateDebut = null;
+        $dateFin = null;
+        $isInscrit = true;
+        $organiser = true;
+        $isNotInscrit = true;
+        $passee = false;
+        $dateDuJour = new \DateTime('now');
+        $dateDuJourMoinsUnMois = $dateDuJour->sub(new  \DateInterval('P1M'));
+        $dateDuJour = new \DateTime('now');
+        $sorties = $sortieRepository->searchSorties($campusSelect, $search, $dateDebut, $dateFin, $isInscrit, $organiser,
+            $isNotInscrit, $passee, $userId, $dateDuJour, $dateDuJourMoinsUnMois);
             //Campus selectionné
         if($formSearch->isSubmitted() && $formSearch->isValid()) {
             // site sélectionné
             $criteres = $formSearch->getData();
             $campusSelect = $campusRepository->find($criteres['campus']);
-            $campusSelect->getNom();
+            $campusSelect = $campusSelect->getId();
+
             $userId = $userInterface->getId();
             $search = $criteres['search'];
             $dateDebut = $criteres['dateDebut'];
@@ -58,7 +78,7 @@ class MainController extends AbstractController
             $passee = $criteres['passee'];
             $dateDuJour = new \DateTime('now');
             $dateDuJourMoinsUnMois = $dateDuJour->sub(new  \DateInterval('P1M'));
-
+            $dateDuJour = new \DateTime('now');
             $sorties = $sortieRepository->searchSorties($campusSelect, $search, $dateDebut, $dateFin, $isInscrit, $organiser,
                                                        $isNotInscrit, $passee, $userId, $dateDuJour, $dateDuJourMoinsUnMois);
 
@@ -76,23 +96,25 @@ class MainController extends AbstractController
         $dateDuJour = new \DateTime('now');
         foreach($sorties as $sortie) {
             //Si la date d'inscrition est inférieur a la date du jour et dateSortie superieur a la date du jour = etat cloturée
-            if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() > $dateDuJour) {
-                $this->changerEtat($sortie, "Cloturee", $manager);
+            if ($sortie->getEtat()->getLibelle() != "Annulée" && $sortie->getEtat()->getLibelle() != "Creer") {
+                if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() > $dateDuJour) {
+                    $this->changerEtat($sortie, "Cloturee", $manager);
 
-            }
+                }
 
-            //Si la date d'inscrition est superieur a la date du jour et la date sortie aussi = etat ouverte
-            if ($sortie->getDateLimiteInscription() > $dateDuJour && $sortie->getDateHeureDebut() > $dateDuJour) {
-                $this->changerEtat($sortie, "Ouverte", $manager);
+                //Si la date d'inscrition est superieur a la date du jour et la date sortie aussi = etat ouverte
+                if ($sortie->getDateLimiteInscription() > $dateDuJour && $sortie->getDateHeureDebut() > $dateDuJour) {
+                    $this->changerEtat($sortie, "Ouverte", $manager);
 
-            }
-            //Si la date d'insciption est inferieur a la date du jour et dateSortie = dateDuJour = etat en cours
-            if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() == $dateDuJour) {
-                $this->changerEtat($sortie, "Activite, en cours", $manager);
-            }
-            //Si la date d'inscription est inférieur a la date du jour et dateSortie aussi = etat passée
-            if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() < $dateDuJour) {
-                $this->changerEtat($sortie, "Passee", $manager);
+                }
+                //Si la date d'insciption est inferieur a la date du jour et dateSortie = dateDuJour = etat en cours
+                if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() == $dateDuJour) {
+                    $this->changerEtat($sortie, "Activite, en cours", $manager);
+                }
+                //Si la date d'inscription est inférieur a la date du jour et dateSortie aussi = etat passée
+                if ($sortie->getDateLimiteInscription() < $dateDuJour && $sortie->getDateHeureDebut() < $dateDuJour) {
+                    $this->changerEtat($sortie, "Passee", $manager);
+                }
             }
         }
     }
